c855376a4b77ab08742c0b36c7741c16aac44315
Cristiano Longo <cristianolongo@hackspacecatania.it>
2019-01-03 22:45:01 +0100

    winston

---
+++ adultsonly/index.php
@@ -0,0 +1,23 @@
+<html lang="it">
+	<head>
+		<title>Aaron Winston Smith - Laboratorio di Informatizzazione per Adulti</title>
+		 <link rel="stylesheet" type="text/css" href="../aws.css">
+	</head>
+	<body>
+		<header>
+			<h1>Laboratorio di Informatizzazione per Adulti</h1>
+			<p class="inciso">Ciao, sono <a href="../index.html">Winston</a> e vengo dal Cyberspazio ...</p>
+			<p><img src="frusta.jpg" alt="a lady and a boy" /></p>
+		</header>
+		<p>Stiamo organizzando un <em>Laboratorio di Informatizzazione</em>
+		presso la <a href="http://palestralupo.org">Palestra Lupo</a>. </p>
+		
+		<p>Aiutaci compilando il questionario, anche se non pensi di partecipare!</p>
+
+		<form>
+			<label for="possiedi">Possiedi un computer?</label>
+			<input type="checkbox" name="possiedofisso">Un computer fisso</input>
+			<input type="checkbox" name="possiedofisso">Un computer portatile</input>
+		</form>
+	</body>
+</html>
