d116bda6b635bf9718ecb9f353a04f7f0597ca51
Cristiano Longo <cristianolongo@hackspacecatania.it>
2019-01-10 12:00:11 +0100

    connect to database

---
+++ associazioniwebsondaggio/record.php
@@ -0,0 +1,27 @@
+<html lang="it">
+	<head>
+		<title>Aaron Winston Smith - web e associazioni culturali - grazie</title>
+		 <link rel="stylesheet" type="text/css" href="../aws.css">
+	<body>
+		<header>
+		<h1>Grazie!</h1> 
+		<p><img src="../turingbig.png" alt="Alan Turing Aged 16" style="width:10em" /></p>
+		</header>
+<?php
+	$db = new mysqli("localhost", "test", "test", "agendalibera");
+	$db->query("create table if not exists web_associazioni_sondaggio (".
+		"created timestamp,".
+		"tipoutente varchar(20),".
+		"modo varchar(20),".
+		"modoaltro text,".
+		"mododesiderata varchar(20),".
+		"mododesiderataaltro text,".
+		"preavviso varchar(20),".
+		"sitoweb varchar(20),".
+		"libero text);");
+	$db->close();
+?>
+		<p>Ovviamente qualcosa bolle in pentola. Se vuoi scrivimi pure alla mail <a href="mailto:aaronwinstonsmith@autistici.org">aaronwinstonsmith@autistici.org</a>
+		oppure cerca <a href="https://www.facebook.com/aaronwinston.smith">Aaron Winston Smith</a> su Facebook.</p> 
+	</body>
+</html>
